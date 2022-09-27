f7d83b5c962ef7ac330c4fe98a5446e19d4cecb5
Arafat Hossain <arafathossain0895@gmail.com>
2022-09-27 16:15:11 +0600

    Added persistence contracts in application project

---
+++ CVBuilder.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace CVBuilder.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<IReadOnlyList<T>> ListAllAsync(Guid employeeId);
+        Task<T> GetByIdAsync(Guid employeeId, int id);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(Guid employeeId, int id);
+        Task<bool> ExistsAsync(Guid employeeId, int id);
+
+    }
+}
---
+++ CVBuilder.Application/Contracts/Persistence/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using CVBuilder.Domain.Entities;
+
+namespace CVBuilder.Application.Contracts.Persistence
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<Employee>> GetAllEmployeesAsync();
+        Task<Employee> AddEmployeeAsync(Employee employee);
+        Task<Employee> GetEmployeeByIdAsync(Guid employeeId);
+        Task UpdateEmployeeAsync(Employee employee);
+        Task DeleteEmployeeAsync(Guid employeeId);
+        Task<bool> EmployeeExistsAsync(Guid employeeId);
+    }
+}
