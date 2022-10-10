f7c1251ae5bf97239ca66cbdd060f6984157a966
Arafat Hossain <arafathossain0895@gmail.com>
2022-10-10 14:50:52 +0600

    42 | Added Model Validation in LoginViewModel

--- CVBuilder.Application/ViewModels/Account/LoginViewModel.cs
+++ CVBuilder.Application/ViewModels/Account/LoginViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace CVBuilder.Application.ViewModels.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CVBuilder.Application.ViewModels.Account
 {
     public class LoginViewModel
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
