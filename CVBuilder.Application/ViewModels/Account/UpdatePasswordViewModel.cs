011abe322b7c2b8db17f74369865c4be0264f289
Arafat Hossain <arafathossain0895@gmail.com>
2022-10-10 14:55:38 +0600

    43 | Added Model Validation in UpdatePasswordViewModel

--- CVBuilder.Application/ViewModels/Account/UpdatePasswordViewModel.cs
+++ CVBuilder.Application/ViewModels/Account/UpdatePasswordViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace CVBuilder.Application.ViewModels.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CVBuilder.Application.ViewModels.Account
 {
     public class UpdatePasswordViewModel
     {
+        [Required]
         public string CurrentPassword { get; set; }
+        [Required]
+        [MinLength(8)]
         public string NewPassword { get; set; }
+        [Required]
+        [Compare("NewPassword")]
         public string ConfirmPassword { get; set; }
     }
 }
