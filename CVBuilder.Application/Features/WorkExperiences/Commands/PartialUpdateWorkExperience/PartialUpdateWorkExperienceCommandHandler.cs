c836ecc8629af5da53b813a2c6e577fde52c148a
Arafat Hossain <arafathossain0895@gmail.com>
2022-10-12 15:29:54 +0600

    69 | Implemented PartialUpdateWorkExperienceCommandHandler

--- CVBuilder.Application/Features/WorkExperiences/Commands/PartialUpdateWorkExperience/PartialUpdateWorkExperienceCommandHandler.cs
+++ CVBuilder.Application/Features/WorkExperiences/Commands/PartialUpdateWorkExperience/PartialUpdateWorkExperienceCommandHandler.cs
@@ -14,12 +14,12 @@ namespace CVBuilder.Application.Features.WorkExperiences.Commands.PartialUpdateW
         }
         public async Task<Unit> Handle(PartialUpdateWorkExperienceCommand request, CancellationToken cancellationToken)
         {
-            var workExperienceDetails = await repository.GetWorkExperienceByIdAsync(request.EmployeeId, request.WorkExperienceId);
+            var workExperienceDetails = await GetWorkExperienceDetails(request.EmployeeId, request.WorkExperienceId);
 
             if (workExperienceDetails == null)
                 throw new Exceptions.NotFoundException(nameof(WorkExperience), request.WorkExperienceId);
 
-            
+
             workExperienceDetails.Designation = request.Designation ?? workExperienceDetails.Designation;
 
             workExperienceDetails.Company = request.Company ?? workExperienceDetails.Company;
@@ -31,6 +31,11 @@ namespace CVBuilder.Application.Features.WorkExperiences.Commands.PartialUpdateW
             await repository.UpdateAsync(workExperienceDetails);
 
             return Unit.Value;
+        }
+
+        private async Task<WorkExperience?> GetWorkExperienceDetails(Guid employeeId, int workExperienceId)
+        {
+            return await repository.GetWorkExperienceByIdAsync(employeeId, workExperienceId);
         }
     }
 }
