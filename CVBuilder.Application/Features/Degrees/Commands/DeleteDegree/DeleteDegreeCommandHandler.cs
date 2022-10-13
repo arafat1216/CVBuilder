5212aaa9a0539ead286d00217327b25dc86bd325
Arafat Hossain <arafathossain0895@gmail.com>
2022-10-13 15:21:05 +0600

    89 | Removed AutoMapper From DeleteDegreeCommandHandler

--- CVBuilder.Application/Features/Degrees/Commands/DeleteDegree/DeleteDegreeCommandHandler.cs
+++ CVBuilder.Application/Features/Degrees/Commands/DeleteDegree/DeleteDegreeCommandHandler.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using CVBuilder.Application.Contracts.Persistence;
+﻿using CVBuilder.Application.Contracts.Persistence;
 using CVBuilder.Domain.Entities;
 using MediatR;
 
@@ -8,12 +7,10 @@ namespace CVBuilder.Application.Features.Degrees.Commands.DeleteDegree
     public class DeleteDegreeCommandHandler : IRequestHandler<DeleteDegreeCommand>
     {
         private readonly IDegreeRepository repository;
-        private readonly IMapper mapper;
 
-        public DeleteDegreeCommandHandler(IDegreeRepository repository, IMapper mapper)
+        public DeleteDegreeCommandHandler(IDegreeRepository repository)
         {
             this.repository = repository;
-            this.mapper = mapper;
         }
         public async Task<Unit> Handle(DeleteDegreeCommand request, CancellationToken cancellationToken)
         {
