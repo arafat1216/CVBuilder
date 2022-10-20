179b77f337d3c037a90dfe98ae4ba7d1df97c5c2
Arafat Hossain <arafathossain0895@gmail.com>
2022-10-20 19:23:04 +0600

    Added Handle Resource Request Command

---
+++ CVBuilder.Application/Features/ResourceRequests/Commands/HandleResourceRequest/UpdateResourceRequestCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace CVBuilder.Application.Features.ResourceRequests.Commands.HandleResourceRequest
+{
+    public class UpdateResourceRequestCommand : IRequest
+    {
+        public int RequestId { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
