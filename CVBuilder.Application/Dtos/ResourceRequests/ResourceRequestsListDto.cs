eca073b56dfb0209ca12631bf248feb7942e64ac
Arafat Hossain <arafathossain0895@gmail.com>
2022-10-20 15:39:10 +0600

    Added Resource Requests List Dto

---
+++ CVBuilder.Application/Dtos/ResourceRequests/ResourceRequestsListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CVBuilder.Application.Dtos.ResourceRequests
+{
+    public class ResourceRequestsListDto
+    {
+        public int RequestId { get; set; }
+        public Guid AppliedBy { get; set; }
+        public string RequestType { get; set; }
+        public string ResourceType { get; set; }
+        public string Reason { get; set; }
+        public string Status { get; set; }
+        public Guid? ReviewedBy { get; set; }
+    }
+}
