461246f922ad9297a1da8143a2ff69797ae69876
Arafat Hossain <arafathossain0895@gmail.com>
2022-10-20 15:38:20 +0600

    Added Requests Controller

---
+++ CVBuilder.Api/Controllers/RequestsController.cs
@@ -0,0 +1,35 @@
+﻿using CVBuilder.Application.Features.ResourceRequests.Queries.ListAllRequests;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVBuilder.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestsController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public RequestsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet("my-requests")]
+        public async Task<IActionResult> GetMyRequests()
+        {
+            var requestDto = new ListAllResourceRequestsQuery();
+
+            var response = await mediator.Send(requestDto);
+
+            return Ok(response);  
+        }
+
+        [HttpPost("{requestId}")]
+        public async Task<IActionResult> PostRequest([FromRoute] int requestId, [FromQuery] bool approve)
+        {
+            return Ok();
+        }
+    }
+}
