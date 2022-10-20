288efe4206b79e009749453a53beb27dc416b97f
Arafat Hossain <arafathossain0895@gmail.com>
2022-10-20 15:39:39 +0600

    Added Resource Requests Feature

---
+++ CVBuilder.Application/Features/ResourceRequests/Queries/ListAllRequests/ListAllResourceRequestsQuery.cs
@@ -0,0 +1,10 @@
+﻿using CVBuilder.Application.Dtos.ResourceRequests;
+using MediatR;
+
+namespace CVBuilder.Application.Features.ResourceRequests.Queries.ListAllRequests
+{
+    public class ListAllResourceRequestsQuery : IRequest<List<ResourceRequestsListDto>>
+    {
+
+    }
+}
---
+++ CVBuilder.Application/Features/ResourceRequests/Queries/ListAllRequests/ListAllResourceRequestsQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using CVBuilder.Application.Contracts.Authentication;
+using CVBuilder.Application.Contracts.Persistence;
+using CVBuilder.Application.Dtos.ResourceRequests;
+using MediatR;
+
+namespace CVBuilder.Application.Features.ResourceRequests.Queries.ListAllRequests
+{
+    public class ListAllResourceRequestsQueryHandler : IRequestHandler<ListAllResourceRequestsQuery, List<ResourceRequestsListDto>>
+    {
+        private readonly IMapper mapper;
+        private readonly IResourceRequestRepository resourceRequestRepository;
+        private readonly IApplicationUser applicationUser;
+
+        public ListAllResourceRequestsQueryHandler(IMapper mapper, IResourceRequestRepository resourceRequestRepository, IApplicationUser applicationUser)
+        {
+            this.mapper = mapper;
+            this.resourceRequestRepository = resourceRequestRepository;
+            this.applicationUser = applicationUser;
+        }
+
+        
+
+        public async Task<List<ResourceRequestsListDto>> Handle(ListAllResourceRequestsQuery request, CancellationToken cancellationToken)
+        {
+            var resourceRequestsList = await resourceRequestRepository.ListAllAsync(r => r.AppliedBy == applicationUser.GetUserId());
+
+            return mapper.Map<List<ResourceRequestsListDto>>(resourceRequestsList);
+        }
+    }
+}
